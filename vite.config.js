ac42770a4dbb98e69790f2e72fa72e706cd92d62
Matthew <118595732+matthewae@users.noreply.github.com>
2025-02-11 17:08:27 +0700

    json add

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path'; // Import the path module
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'), // Add this if you are using bootstrap
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/js/app.js',
+                'resources/css/app.css',
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // This is the crucial part!
+        manifest: true, // Important for Laravel integration
+    },
 });
